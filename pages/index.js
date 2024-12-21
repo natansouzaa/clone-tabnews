f99b120932e249c1d762224ec8dee5655a2253c6
Natan Souza <natan.souza@ccc.ufcg.edu.br>
2024-12-21 22:56:53 +0000

    add initial page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Atenção, esse site está sendo construído por você, para melhorar a sua e como consequência a minha vida...</h1>
+            <h2>Eu amo você!!!</h2>
+        </div>
+    )
+}
+
+export default Home;
