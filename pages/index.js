d6dedaaa2db2d571437422b869653cc1e859d9b8
Natan Souza <natan.souza@ccc.ufcg.edu.br>
2025-01-26 15:55:29 +0000

    add `lint:check` and `lint:fix` scripts

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 function Home() {
-    return (
-        <div>
-            <h1>Atenção, esse site está sendo construído por você, para melhorar a sua e como consequência a minha vida...</h1>
-            <h2>Eu amo você!!!</h2>
-            <h3>🫶</h3>
-        </div>
-    )
+  return (
+    <div>
+      <h1>
+        Atenção, esse site está sendo construído por você, para melhorar a sua e
+        como consequência a minha vida...
+      </h1>
+      <h2>Eu amo você!!!</h2>
+      <h3>🫶</h3>
+    </div>
+  );
 }
 
 export default Home;
